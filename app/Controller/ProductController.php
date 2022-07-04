eea11c486e35f1accda0741e2b742b041e4a78be
Гусельников Денис <guselnikov@intervolga.ru>
2022-07-05 01:16:20 +0300

    Добавил вывод товаров

--- app/Controller/ProductController.php
+++ app/Controller/ProductController.php
@@ -3,13 +3,52 @@
 namespace App\Controller;
 
 use App\Controller;
+use App\Tables\ProductTable;
+use App\Tables\SectionTable;
 use App\ViewManager;
 
 class ProductController extends Controller
 {
 	public static function show()
 	{
-		ViewManager::show('header', ['title' => 'Заказы']);
+		ViewManager::show('header', ['title' => 'Товары']);
+
+		$result['currentUrl'] = $_SERVER['REQUEST_URI'];
+		$result['result'] = [
+			'columns' => [
+				'ID', 'Название', 'Категория', 'Цена'
+			]
+		];
+
+		$ob = ProductTable::query()
+			->registerRuntimeField('SECTION', [
+				'data_class' => SectionTable::class,
+				'reference' => [
+					'this' => 'SECTION_ID',
+					'ref' => 'ID',
+				],
+				'join_type' => 'inner'
+			])
+			->addSelect('product.ID', 'PRODUCT_ID')
+			->addSelect('product.NAME', 'PRODUCT_NAME')
+			->addSelect('SECTION.NAME', 'SECTION_NAME')
+			->addSelect('product.PRICE', 'PRODUCT_PRICE');
+
+		$query = $ob->getQuery();
+		$users = $ob->exec();
+
+		while ($itm = $users->fetch())
+		{
+			$result['result']['items'][] = [
+				'PRODUCT_ID' => $itm['PRODUCT_ID'],
+				'PRODUCT_NAME' => $itm['PRODUCT_NAME'],
+				'SECTION_NAME' => $itm['SECTION_NAME'],
+				'PRODUCT_PRICE' => $itm['PRODUCT_PRICE']
+			];
+		}
+
+		ViewManager::show('query', ['query' => [$query]]);
+		ViewManager::show('table', $result);
 		ViewManager::show('footer');
 		return '';
 	}
