fe0131e64943f3d7ef5aa9b8f809172b1b93b8c2
Гусельников Денис <guselnikov@intervolga.ru>
2022-07-05 01:08:59 +0300

    Добавил вывод пунктов выдачи

--- app/Controller/PickPointController.php
+++ app/Controller/PickPointController.php
@@ -3,13 +3,39 @@
 namespace App\Controller;
 
 use App\Controller;
+use App\Tables\PickPointTable;
 use App\ViewManager;
 
 class PickPointController extends Controller
 {
 	public static function show()
 	{
-		ViewManager::show('header', ['title' => 'Заказы']);
+		ViewManager::show('header', ['title' => 'Пункты выдачи']);
+
+		$result['currentUrl'] = $_SERVER['REQUEST_URI'];
+		$result['result'] = [
+			'columns' => [
+				'ID', 'Адрес',
+			]
+		];
+
+		$ob = PickPointTable::query()
+			->addSelect('ID')
+			->addSelect('ADDRESS');
+
+		$query = $ob->getQuery();
+		$users = $ob->exec();
+
+		while ($itm = $users->fetch())
+		{
+			$result['result']['items'][] = [
+				'ID' => $itm['pick_point_ID_ALIAS'],
+				'ADDRESS' => $itm['pick_point_ADDRESS_ALIAS'],
+			];
+		}
+
+		ViewManager::show('query', ['query' => [$query]]);
+		ViewManager::show('table', $result);
 		ViewManager::show('footer');
 		return '';
 	}
